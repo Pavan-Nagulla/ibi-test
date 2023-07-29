24db46b1048ddb1e0f426388da99c3396aa65611
pavannagulla <pavankalyannagulla28@gmail.com>
2023-07-29 10:21:51 +0530

    updated

--- code.js
+++ code.js
@@ -12,3 +12,16 @@ for (var i = 0; i < 5; i++) {
 //5
 //5
 //5
+
+/*  
+
+Explanation:
+
+This behavior is due to the asynchronous nature of the setTimeout function. When the loop runs, it schedules five timeouts almost simultaneously, each set to execute after 100 milliseconds. The setTimeout function does not block the loop; instead, it returns immediately, allowing the loop to continue its iterations.
+
+By the time the first console.log(i) executes, the value of i has already been incremented to 5, as the loop runs from 0 to 4. The loop doesn't wait for the setTimeout to finish; it completes all its iterations rapidly.
+
+After 100 milliseconds have passed, the callback function inside the setTimeout is triggered for each of the scheduled timeouts. However, since i has already reached 5 after the loop finished, all five callbacks log the value of i, which is now 5.
+
+
+*/
